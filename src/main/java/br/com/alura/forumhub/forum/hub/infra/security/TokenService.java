c32b2cb15df841c115efa2d7b9887014048be574
WellsJhones <wellsjhones@gmail.com>
2024-07-06 20:48:50 -0400

    improving security

--- src/main/java/br/com/alura/forumhub/forum/hub/infra/security/TokenService.java
+++ src/main/java/br/com/alura/forumhub/forum/hub/infra/security/TokenService.java
@@ -4,6 +4,7 @@ import br.com.alura.forumhub.forum.hub.domain.usuario.Usuario;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
 import org.springframework.stereotype.Service;
 
 import java.time.Instant;
@@ -16,6 +17,7 @@ public class TokenService {
     public String gerarToken(Usuario usuario) {
         try {
             var algoritmo = Algorithm.HMAC256("12345678");
+            System.out.println(usuario.getEmail());
             return JWT.create()
                     .withIssuer("API Forum_hub")
                     .withSubject(usuario.getEmail())
@@ -23,6 +25,18 @@ public class TokenService {
                     .sign(algoritmo);
         } catch (JWTCreationException exception){
             throw new RuntimeException("erro ao gerrar token jwt", exception);
+        }
+    }
+    public String getSubject(String tokenJWT){
+        try {
+            var algoritmo = Algorithm.HMAC256("12345678");
+            return JWT.require(algoritmo)
+                    .withIssuer("API Forum_hub")
+                    .build()
+                    .verify(tokenJWT)
+                    .getSubject();
+        } catch (JWTVerificationException exception) {
+            throw new RuntimeException("Token JWT inválido ou expirado!");
         }
     }
 
